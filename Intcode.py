cc3799ff666045189f377258b26e73b364ebbd26
syntaxaire <syntaxaire@gmail.com>
2019-12-08 23:19:10 -0500

    Update Intcode VM and add many more tests

--- Intcode.py
+++ Intcode.py
@@ -3,16 +3,24 @@ from typing import List, Tuple
 # opcodes for the Intcode machine
 ADD = 1
 MULTIPLY = 2
-STORE = 3
-RETRIEVE = 4
+INPUT = 3
+OUTPUT = 4
+JUMPIFTRUE = 5
+JUMPIFFALSE = 6
+LESSTHAN = 7
+EQUALS = 8
 HALT = 99
 
 # sequence lengths for the Intcode machine, including the opcode itself
 OPCODE_LENGTHS = {ADD: 4,
                   MULTIPLY: 4,
-                  STORE: 2,
-                  RETRIEVE: 2,
+                  INPUT: 2,
+                  OUTPUT: 2,
                   HALT: 1,
+                  JUMPIFTRUE: 3,
+                  JUMPIFFALSE: 3,
+                  LESSTHAN: 4,
+                  EQUALS: 4,
                   }
 
 # parameter modes for the Intcode machine
@@ -21,13 +29,15 @@ IMMEDIATE = 1
 
 
 class IntcodeVM:
-    def __init__(self, memory: List[int], inputs: list, outputs: list):
+    def __init__(self, memory: List[int], inputs: list, outputs: list, debug=False):
         self.memory = memory
         self.inputs = inputs
         self.outputs = outputs
+        self.debug = debug
 
     def decode(self, addr) -> Tuple[int, List[int]]:
-        """Return the opcode at the given address, and the list of modes it needs to operate."""
+        """Return the opcode at the given address, and the list of parameter modes it may need to
+        operate. Fills any parameter mode that is not explicitly given with the default."""
         instruction = str(self.memory[addr])  # e.g. '1002'
         opcode = int(instruction[-2:])
         remainder = list(instruction[:-2])  # may be [] if a bare opcode was given (e.g. '2')
@@ -51,23 +61,56 @@ class IntcodeVM:
 
     def run(self):
         ip = 0
+        jumped = False
         opcode, modes = self.decode(ip)
-        while opcode != HALT:
-            if opcode == ADD:
+        while opcode != HALT:  # code 99
+            if opcode == ADD:  # code 1
                 a = self.read(ip+1, modes.pop(0))
                 b = self.read(ip+2, modes.pop(0))
                 addr = self.read(ip+3)
                 self.memory[addr] = a + b
-            elif opcode == MULTIPLY:
+            elif opcode == MULTIPLY:  # code 2
                 a = self.read(ip+1, modes.pop(0))
                 b = self.read(ip+2, modes.pop(0))
                 addr = self.read(ip+3)
                 self.memory[addr] = a * b
-            elif opcode == STORE:
+            elif opcode == INPUT:  # code 3
                 addr = self.read(ip+1)
-                self.memory[addr] = self.inputs.pop(0)
-            elif opcode == RETRIEVE:
-                addr = self.read(ip+1)
-                self.outputs.append(self.memory[addr])
-            ip += OPCODE_LENGTHS[opcode]
+                val = self.inputs.pop(0)
+                self.memory[addr] = val
+            elif opcode == OUTPUT:  # code 4
+                val = self.read(ip+1, modes.pop(0))
+                self.outputs.append(val)
+            elif opcode == JUMPIFTRUE:  # code 5
+                val = self.read(ip+1, modes.pop(0))
+                addr = self.read(ip+2, modes.pop(0))
+                if val != 0:
+                    ip = addr
+                    jumped = True
+            elif opcode == JUMPIFFALSE:  # code 6
+                val = self.read(ip+1, modes.pop(0))
+                addr = self.read(ip+2, modes.pop(0))
+                if val == 0:
+                    ip = addr
+                    jumped = True
+            elif opcode == LESSTHAN:  # code 7
+                a = self.read(ip+1, modes.pop(0))
+                b = self.read(ip+2, modes.pop(0))
+                addr = self.read(ip+3)
+                if a < b:
+                    self.memory[addr] = 1
+                else:
+                    self.memory[addr] = 0
+            elif opcode == EQUALS:  # code 8
+                a = self.read(ip + 1, modes.pop(0))
+                b = self.read(ip + 2, modes.pop(0))
+                addr = self.read(ip + 3)
+                if a == b:
+                    self.memory[addr] = 1
+                else:
+                    self.memory[addr] = 0
+            if not jumped:
+                ip += OPCODE_LENGTHS[opcode]
+            else:
+                jumped = False
             opcode, modes = self.decode(ip)
